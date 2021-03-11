e5e825f9ab2d375b33dd861b87314e54a3ae5c7f
Diego Ferraz <diego.ferraz@invillia.com>
2021-03-11 16:44:19 -0300

    adds event detail page

--- pages/events/[eventID].js
+++ pages/events/[eventID].js
@@ -1,7 +1,33 @@
+import { useRouter } from 'next/router'
+
+import { getEventById } from '../../dummy-data'
+
+import EventSummary from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+
 const EventDetail = () => {
+  const router = useRouter()
+
+  const eventID = router.query.eventID
+  const eventData = getEventById(eventID)
+
+  if (!eventData) return <p>No event found!</p>
+
   return (
-    <h1>Event detail page</h1>
+    <>
+      <EventSummary title={eventData.title} />
+      <EventLogistics
+        date={eventData.date}
+        address={eventData.location}
+        image={eventData.image}
+        imageAlt={eventData.title}
+      />
+      <EventContent>
+        <p>{eventData.description}</p>
+      </EventContent>
+    </>
   )
 }
  
-export default EventDetail;
+export default EventDetail
