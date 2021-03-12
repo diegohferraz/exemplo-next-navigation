8e197d08398c65def171df0638460cb86166afd0
Diego Ferraz <diego.ferraz@invillia.com>
2021-03-12 14:17:25 -0300

    adds filter functionality

--- pages/events/[...slugs].js
+++ pages/events/[...slugs].js
@@ -1,6 +1,43 @@
+import { useRouter } from 'next/router'
+
+import {getFilteredEvents} from '../../dummy-data'
+
+import EventList from '../../components/events/event-list'
+import ResultsTitle from '../../components/results-title/results-title'
+
 const FilteredEventsPage = () => {
+  const router = useRouter()
+
+  const filterData = router.query.slugs
+
+  if (!filterData) return <p className='center'>Loading...</p>
+
+  const filteredYear = +filterData[0]
+  const filteredMonth = +filterData[1]
+
+  if (
+    isNaN(filteredYear) || 
+    isNaN(filteredMonth) ||
+    filteredYear < 2021 ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
+    return <p className='center'>Ivalid Filters</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0) return <p className='center'>No Events Found</p>
+
+  const date = new Date(filteredYear, filteredMonth - 1)
   return (
-    <h1>Filtered events page</h1>
+    <div>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </div>
   )
 }
  
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,20 @@
+import { useRouter } from 'next/router'
 import { getAllEvents } from '../../dummy-data'
+
 import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
 
 const Events = () => {
+  const router = useRouter()
   const featuredEvents = getAllEvents()
+
+  function findEventesHandler(year, month) {
+    router.push(`/events/${year}/${month}`)
+  }
 
   return (
     <div>
+      <EventsSearch onSearch={findEventesHandler} />
       <EventList items={featuredEvents} />
     </div>
   )
