7e8b9045fd17e37021b66f61614ae7f0b3e04fb1
Diego Ferraz <diego.ferraz@invillia.com>
2021-06-01 11:56:45 -0300

    alters slugs page to use stale while revalidade strategy instead of SSR

--- pages/events/[...slugs].js
+++ pages/events/[...slugs].js
@@ -1,34 +1,35 @@
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import useSWR from 'swr'
 
-import { getFilteredEvents } from '../../helpers/api-utils'
+// import { getFilteredEvents } from '../../helpers/api-utils'
 
 import EventList from '../../components/events/event-list'
 import ResultsTitle from '../../components/results-title/results-title'
 
 const FilteredEventsPage = ({ events, hasError, filterDate }) => {
+  const [loadedEvents, setLoadedEvents ] = useState()
   const router = useRouter()
+  const filterData = router.query.slugs
 
-  if (!events) return <p className='center'>Loading...</p>
+  const { data, error } = useSWR('https://estudos-dd8db.firebaseio.com/events.json')
 
-  if (hasError) {
-    return <p className='center'>Ivalid Filters</p>
-  }
+  useEffect(() => {
+    if (data) {
+      const events = []
 
-  if (!events || events.length === 0) return <p className='center'>No Events Found</p>
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key]
+        })
+      }
 
-  const date = new Date(filterDate.year, filterDate.month - 1)
-  return (
-    <div>
-      <ResultsTitle date={date} />
-      <EventList items={events} />
-    </div>
-  )
-}
+      setLoadedEvents(events)
+    }
+  }, [data])
 
-export async function getServerSideProps(context) {
-  const { params } = context
-
-  const filterData = params.slugs
+  if (!loadedEvents) return <p className='center'>Loading...</p>
 
   const filteredYear = +filterData[0]
   const filteredMonth = +filterData[1]
@@ -38,32 +39,66 @@ export async function getServerSideProps(context) {
     isNaN(filteredMonth) ||
     filteredYear < 2021 ||
     filteredMonth < 1 ||
-    filteredMonth > 12
-  ) {
-    return {
-      props: { hasError: true }
-      // notFound: true,
-      // redirect: {
-      //   destination: '/error'
-      // }
-    }
-  }
+    filteredMonth > 12 ||
+    error
+  ) return <p className='center'>Ivalid Filters</p>
 
-  const filteredEvents = await getFilteredEvents({
-    year: filteredYear,
-    month: filteredMonth
-  })
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return eventDate.getFullYear() === filteredYear && eventDate.getMonth() === filteredMonth - 1;
+  });
 
-  return {
-    props: {
-      events: filteredEvents,
-      filterDate: {
-        year: filteredYear,
-        month: filteredMonth
-      }
-    }
-  }
 
+  if (!filteredEvents || filteredEvents.length === 0) return <p className='center'>No Events Found</p>
+
+  const date = new Date(filteredYear, filteredMonth - 1)
+  return (
+    <div>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </div>
+  )
 }
+
+// export async function getServerSideProps(context) {
+//   const { params } = context
+
+//   const filterData = params.slugs
+
+//   const filteredYear = +filterData[0]
+//   const filteredMonth = +filterData[1]
+
+//   if (
+//     isNaN(filteredYear) || 
+//     isNaN(filteredMonth) ||
+//     filteredYear < 2021 ||
+//     filteredMonth < 1 ||
+//     filteredMonth > 12
+//   ) {
+//     return {
+//       props: { hasError: true }
+//       // notFound: true,
+//       // redirect: {
+//       //   destination: '/error'
+//       // }
+//     }
+//   }
+
+//   const filteredEvents = await getFilteredEvents({
+//     year: filteredYear,
+//     month: filteredMonth
+//   })
+
+//   return {
+//     props: {
+//       events: filteredEvents,
+//       filterDate: {
+//         year: filteredYear,
+//         month: filteredMonth
+//       }
+//     }
+//   }
+
+// }
  
 export default FilteredEventsPage;
