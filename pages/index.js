8a3c0eecfe7e2a78d166e92940352cf897e0268c
Diego Hernandez Ferraz <diego.ferraz@MacBook-Pro-de-Invillia.local>
2021-06-01 09:50:59 -0300

    adds static generation to index page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import { getFeaturedEvents } from '../dummy-data'
+import { getFeaturedEvents } from '../helpers/api-utils'
 import EventList from '../components/events/event-list'
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents()
-
+const HomePage = ({ events }) => {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents()
+
+  return {
+    props: {
+      events: featuredEvents
+    }
+  }
 }
  
 export default HomePage
